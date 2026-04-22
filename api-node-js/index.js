059e1b36fa4efbc536abf44cfc7b016f3ea24d02
Severino <snsf2134@gmail.com>
2026-04-22 00:20:20 -0300

    Adiciona a API feita utilizando Node.js

---
+++ api-node-js/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+app.use (
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Oi Express'})
+})
+
+app.listen(3000)
